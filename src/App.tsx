b9c9d2c0912d529940e9e911ea938187000a462a
demilv <gonzalo.cano@bootcamp-upgrade.com>
2024-10-31 15:45:45 +0100

    header estructura

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,25 @@
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="mainContainer">
+      <div className="header">
+        <h2 className="title"></h2>
+        <div className="menuButtons">
+          <button>Productos</button>
+          <button>Tienda</button>
+          <button>Soporte</button>
+          <button>Log In</button>
+        </div>
+      </div>
+
+      <div className="body">
+
+      </div>
+
+      <div className="footer">
+
+      </div>
+    </div>
   )
 }
 
