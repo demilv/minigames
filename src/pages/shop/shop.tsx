7a207b4eb64bd788ec8ad23d5ec2867dfcadf779
demilv <gonzalo.cano@bootcamp-upgrade.com>
2024-11-03 19:11:37 +0100

    Estructura basica de la tienda

--- src/pages/shop/shop.tsx
+++ src/pages/shop/shop.tsx
@@ -4,7 +4,39 @@ function Shop (){
 
     return (
         <>
-        
+            <div className="producto">
+                <div className="producto__ladoInfo">
+                    <h1></h1>
+                    <h5></h5>
+                    <div className="producto__ladoInfo__acciones">
+                        <button> Buy!</button>
+                        <button> Try it!</button>
+                    </div>
+                </div>
+                <img />
+            </div>
+            <div className="producto--inverso">
+            <div className="producto__ladoInfo">
+                    <h1></h1>
+                    <h5></h5>
+                    <div className="producto__ladoInfo__acciones">
+                        <button> Buy!</button>
+                        <button> Try it!</button>
+                    </div>
+                </div>
+                <img />
+            </div>
+            <div className="producto">
+            <div className="producto__ladoInfo">
+                    <h1></h1>
+                    <h5></h5>
+                    <div className="producto__ladoInfo__acciones">
+                        <button> Buy!</button>
+                        <button> Try it!</button>
+                    </div>
+                </div>
+                <img />
+            </div>
         </>
     )
 
