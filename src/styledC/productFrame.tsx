cdc259957d3110b0dd6c6a7ea2a5e99c97167fdb
demilv <gonzalo.cano@bootcamp-upgrade.com>
2024-11-05 15:46:07 +0100

    añadido el frame de producto

---
+++ src/styledC/productFrame.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const productFrame = styled.div<{inverse: boolean}>`
+    height:30em;
+    width: 100%;
+    background-color: black;
+    display: flex;
+    justify-content: ${props => props.inverse ? 'flex-end' : 'start'};
+
+`
