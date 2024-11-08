8f495ab3aebf8d959a05d8262e5f58d4eabcd340
demilv <gonzalo.cano@bootcamp-upgrade.com>
2024-11-08 13:01:45 +0100

    mainContainer part 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Shop from './pages/shop/shop';
 import Support from './pages/support/support';
 import Login from './pages/login/login';
-import { MainContainer } from './styledC/mainContainer';
+import { HeaderContainer, Title, Logo, TitleImg } from './styledC/Header';
 
 
 function App() {
@@ -10,14 +10,17 @@ function App() {
   return (
     <Router>
       <div className="mainContainer">
-        <MainContainer> 
-          <h2 className="title">Minijuegos TX</h2>
+        <HeaderContainer> 
+          <Logo>
+            <TitleImg></TitleImg>
+            <Title>Minijuegos TX</Title>
+          </Logo>
             <div className="menuButtons">
               <Link to='/'><button>Tienda</button></Link>
               <Link to ='/support'><button>Soporte</button></Link>
               <Link to='/logIn'><button>Log In</button></Link>
             </div>
-        </MainContainer>
+        </HeaderContainer>
 
         <div className="body">
           <Routes>
---
+++ src/styledC/Header.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const HeaderContainer= styled.div`
+    height:6em;
+    width: 100%;
+    background-color: #1c1c1c;
+    display: flex;
+    flex-direction: row;
+    justify-content:space-between;
+    padding: 0 4em;
+    align-items:center;
+`
+
+export const Title = styled.h2`
+    font-family:"Press Start 2P";
+    color: #fbfbfb;
+    font-size: 1em;
+`
+
+export const TitleImg = styled.img`
+
+`
+
+export const Logo = styled.div`
+
+`
