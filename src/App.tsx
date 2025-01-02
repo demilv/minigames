81dd11ea0aacd932b458eb2ad05e5ef7bab98a21
demilv <gonzalo.cano@bootcamp-upgrade.com>
2025-01-02 12:53:46 +0100

    Resto de elementos del footer styled

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Shop from './pages/shop/shop';
 import Support from './pages/support/support';
 import Login from './pages/login/login';
 import { StyledHeaderContainer, StyledTitle, StyledLogo, StyledTitleImg, StyledButtonLogo } from './styledC/Header';
-import { FooterArea } from './styledC/Footer';
+import { FooterArea, FooterLinks, FooterThings } from './styledC/Footer';
 
 
 function App() {
@@ -33,7 +33,16 @@ function App() {
         </div>       
 
         <FooterArea>
-
+          <FooterThings>
+            <FooterLinks></FooterLinks>
+            <FooterLinks></FooterLinks>
+            <FooterLinks></FooterLinks>
+          </FooterThings>
+          <FooterThings>
+            <FooterLinks></FooterLinks>
+            <FooterLinks></FooterLinks>
+            <FooterLinks></FooterLinks>
+          </FooterThings>
         </FooterArea>
       </div>
 
--- src/styledC/Footer.tsx
+++ src/styledC/Footer.tsx
@@ -9,3 +9,15 @@ export const FooterArea = styled.div `
     width:100%;
     padding: 0 4em;
 `
+
+export const FooterThings = styled.div `
+    width: 8em;
+    display:flex;
+    flex-direction:column
+`
+
+export const FooterLinks = styled.h5`
+    color:rgb(222, 206, 206);
+    font-size: 1.5em;
+    font-family: 'Garamond'
+`
