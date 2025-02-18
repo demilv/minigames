07e703906866e5a47bbe7a6496aec500fec8655a
demilv <gonzalo.cano@bootcamp-upgrade.com>
2025-02-18 14:30:45 +0100

    Contexto del usuario

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -16,4 +16,48 @@ import React from "react";
     children: React.ReactNode;
   }
   
+  type Action = | { type: 'SET_USERDATA'; payload: { name: string; email: string; pass: string } } | { type: 'LOGOUT' };
   
+  interface UserContextType {
+    state: State;
+    dispatch: React.Dispatch<Action>;
+  }
+
+const userReducer = (state: State, action:Action) => {
+    switch (action.type) {
+        case "SET_USERDATA":
+            return { ...state, 
+                user:{
+                        name:action.payload.name,
+                        email: action.payload.email,
+                        pass: action.payload.pass,
+                        autenticado: true
+                    }};
+        case "LOGOUT":
+            return {...state, user:{
+                name:null,
+                email: null,
+                pass: null,
+                autenticado: false
+            }};
+                    
+        default:
+            return state;
+    }
+}
+
+export const UserContext = createContext<UserContextType | null>(null);
+    export const UserContextProvider: React.FC<UserContextProviderProps> = ({ children }) => {
+        const [state, dispatch] = useReducer(userReducer, {user:{
+        name:null,
+        email: null,
+        pass: null,
+        autenticado: false
+    }});
+
+    return (
+        <UserContext.Provider value={{state , dispatch}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
