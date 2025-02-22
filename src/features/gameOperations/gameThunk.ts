983a5308d3c3030d4dc403ec1fcfb0caa06d6b1b
demilv <gonzalo.cano@bootcamp-upgrade.com>
2025-02-22 14:13:37 +0100

    gameThunk

---
+++ src/features/gameOperations/gameThunk.ts
@@ -0,0 +1,34 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+export const gamesThunk = createAsyncThunk('games/gamesThunk', async (_, { rejectWithValue }) => {
+    try {
+        const MIAPI = import.meta.env.VITE_MIAPI;
+
+        const token = localStorage.getItem('authorization');
+
+        if (!token) {
+            return rejectWithValue('No se encontró el token de autenticación');
+        }
+
+        const response = await fetch(`${MIAPI}/games`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error('No se encuentran los games');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        if (error instanceof Error) {
+            return rejectWithValue(error.message);
+        } else {
+            return rejectWithValue('Error desconocido');
+        }
+    }
+});
