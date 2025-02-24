ca62840afc210af7762805cbdc7de4daf011cc95
demilv <gonzalo.cano@bootcamp-upgrade.com>
2025-02-24 17:20:16 +0100

    Casi scoreSlice hecho, corregidos algunos errores

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import gameReducer from '../features/gameOperations/gameSlice';
+import userReducer from '../features/userOperations/userSlice';
+import scoreReducer from '../features/scoreOperations/scoreSlice';
 
 export const store = configureStore({
     reducer: {
---
+++ src/features/scoreOperations/scoreSlice.ts
@@ -0,0 +1,47 @@
+import { scoresThunk } from './scoreThunk';
+import { createSlice } from '@reduxjs/toolkit'
+import type { RootState } from '../../app/store';
+import { Score } from '../types/interfaces';
+
+interface interfaceState {
+    scores: Score[],
+    status: 'idle' | 'pending' | 'fulfilled' | 'rejected',
+    error: string | null
+}
+
+const initialState: interfaceState = {
+    scores: [],
+    status: 'idle', 
+    error: null,
+}
+
+export const scoresSlice = createSlice({
+    name: 'scores',
+    initialState,
+    reducers: {
+        addScore: (state, action) => {
+
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(scoresThunk.pending, (state) => {
+                state.status = 'pending';
+            })
+            .addCase(scoresThunk.fulfilled, (state, action) => {
+                state.status = 'fulfilled';
+                state.scores = action.payload as Score[];
+            })
+            .addCase(scoresThunk.rejected, (state, action) => {
+                state.status = 'rejected';
+                state.error = action.payload as string;
+            });
+    }
+});
+
+export const {  } = scoresSlice.actions;
+export const scoresDataSelect = (state: RootState) => state.scores.scores;
+export const scoresStatusSelect = (state: RootState) => state.scores.status;
+export const scoresErrorSelect = (state: RootState) => state.scores.error;
+
+export default scoresSlice.reducer;
