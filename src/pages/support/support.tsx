65a3fb3f89622210a609c81a2e8fad3d22a7996c
demilv <gonzalo.cano@bootcamp-upgrade.com>
2025-03-17 13:20:04 +0100

    Creados styled components

--- src/pages/support/support.tsx
+++ src/pages/support/support.tsx
@@ -1,7 +1,20 @@
+import { StyledSupportDiv, StyledSupportP } from "../../styledC/support/support";
+
 function Support () {
 
     return (
         <>
+            <StyledSupportDiv>
+                <StyledSupportP>
+                Necesitas ayuda? Estamos aquí para asistirte.
+
+                Si tienes alguna pregunta o problema contáctanos a través de los siguientes canales:
+
+                📧 Correo electrónico: gonzalo.cano.rodriguez93@gmail.com
+                📞 Teléfono: +34 622226147
+                💬 Disponible todos los días para hablar.
+                </StyledSupportP>
+            </StyledSupportDiv>
         </>
     )
 }
