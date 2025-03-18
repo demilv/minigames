332d4db3037673f6a0bac9b150012a1798b99efd
demilv <gonzalo.cano@bootcamp-upgrade.com>
2025-03-18 23:23:06 +0100

    Componente BlueVersion para los visitantes

---
+++ src/styledC/version/version.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const BlueVersion = styled.div`
+    width:100%;
+    height:100%;
+    padding: 2em;
+    background-color:rgb(128, 242, 213);
+    color:rgb(255, 98, 0);
+    font-family: "VT323"
+`
