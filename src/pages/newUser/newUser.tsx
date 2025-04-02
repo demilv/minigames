b60a1af235e7d6abf15090783a028b8ed2d929f3
demilv <gonzalo.cano@bootcamp-upgrade.com>
2025-04-02 18:21:14 +0200

    Reglas de validación

--- src/pages/newUser/newUser.tsx
+++ src/pages/newUser/newUser.tsx
@@ -12,15 +12,35 @@ interface newUserData {
     email: string,
     phone: string
 }
+interface rules{
+  rule1?: boolean,
+  rule2?: boolean,
+  rule3?: boolean,
+  rule4?: boolean
+}
 
 const NewUser = () => {
 
     const [formData, setFormData] = useState<newUserData>({name: '', pass: '', email:'', phone:''})
+    const [rules, setRules] = useState<rules>({rule1: undefined, rule2: undefined, rule3: undefined, rule4: undefined })
 
     const navigate = useNavigate()
 
     const submitForm = async (ev: React.FormEvent<HTMLFormElement>) => {
         ev.preventDefault();
+
+        if (!Object.values(rules).every(rule => rule)) {
+          Swal.fire({
+              title: "Error",
+              text: "Asegurate de que todos los campos son correctos.",
+              icon: "error",
+              timer: 3000,
+              timerProgressBar: true,
+              showConfirmButton: true
+          });
+          return;
+      }
+
         console.log("probando datos")
         const newUserPath = `${import.meta.env.VITE_MIAPI}/newUser`;
         const token = localStorage.getItem('authorization');
@@ -47,7 +67,7 @@ const NewUser = () => {
 
           Swal.fire({
             title: "Good job!",
-            text: "User added successfully!",
+            text: "¡Usuario creado!",
             icon: "success",
             timer: 3000,
             timerProgressBar: true,
@@ -59,8 +79,33 @@ const NewUser = () => {
 
     const changeData = async (ev: React.ChangeEvent<HTMLInputElement>) => {
         const {name, value} = ev.target;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         setFormData ({...formData, [name]: value})
-
+        switch (name)
+        {
+          case 'name':
+            if (name.length <3 || name.length > 15)
+              setRules(prevRules => ({ ...prevRules, rule1: false }));
+            else setRules(prevRules => ({ ...prevRules, rule1: true }));
+            return;
+          case 'pass':
+            if (name.length <3 || name.length > 15)
+              setRules(prevRules => ({ ...prevRules, rule2: false }));
+            else setRules(prevRules => ({ ...prevRules, rule2: true }));
+            return;
+          case 'email':
+            if (!emailRegex.test(value)) {
+              setRules(prevRules => ({ ...prevRules, rule3: false }));
+            } else {
+              setRules(prevRules => ({ ...prevRules, rule3: true }));
+            }
+            return;
+          case 'phone':
+          if (name.length !== 9)
+            setRules(prevRules => ({ ...prevRules, rule4: false }));
+          else setRules(prevRules => ({ ...prevRules, rule4: true }));
+          return;
+        }
     }
 
     return (
