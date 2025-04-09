ea47e3e7e420a61f88537b6c2a1427e5340f8eb2
demilv <gonzalo.cano@bootcamp-upgrade.com>
2025-04-09 03:26:22 +0200

    preparando juegos para mostrarlos todos

--- src/pages/shop/shop.tsx
+++ src/pages/shop/shop.tsx
@@ -1,38 +1,37 @@
 import { StyledInstructionsDiv, StyledLadoInfo, StyledProductFrame, StyledQuickDescription, StyledSIImg, StyledSITitle, StyledTitleProduct } from "../../styledC/shop/productFrame";
 import { StyledMain } from "../../styledC/generic/Screens";
 import { MUIButton, MUIButtonGroup } from "../../styledC/generic/MUIButtons";
-import Trial1 from "./trial1";
 import { useState } from "react";
 import { OpenChatButton } from "../../styledC/shop/Chat";
 import ChatBubble from "./chatBubble";
+import { gamesDataSelect } from "../../features/gameOperations/gameSlice";
+import { useSelector } from "react-redux";
 
 function Shop (){
 
     const [isChatOpen, setChatOpen] = useState(false);
-    const [isTrial1Open, setTrial1Open] = useState(false);    
-
-    const openTrial1 = () => setTrial1Open(true); 
-    const closeTrial1 = () => setTrial1Open(false);
     const openChat = () => setChatOpen(true); 
     const closeChat = () => setChatOpen(false);
+    const gameData = useSelector(gamesDataSelect)
 
-    return (
+
+
+    return gameData.map((games, index) => (
         <>
-            {isTrial1Open && <Trial1 closeTrial1={closeTrial1} />}
             <StyledMain>
             {isChatOpen && <ChatBubble closeChat={closeChat}/>}
-                <OpenChatButton onClick={openChat} src="bubbleChat.png" />
+                <OpenChatButton onClick={openChat} src="bubbleChat.png" />  
+                {games.map((game) => (              
                 <StyledProductFrame back={"Leonardo_Phoenix_tic_tac_toe_with_3_rows_and_3_columns_there_s_2.jpg"}>
                     <StyledInstructionsDiv>
-                        <StyledSITitle>Ranking</StyledSITitle>
+                        <StyledSITitle>Reviews</StyledSITitle>
                         <StyledSIImg src="scroll.png"/>
                     </StyledInstructionsDiv>
                     <StyledLadoInfo>
                         <StyledTitleProduct>Ultimate TTT</StyledTitleProduct>
                         <StyledQuickDescription>Quick up to 2-player matches where the winner takes it all!</StyledQuickDescription>
                         <MUIButtonGroup>
-                            <MUIButton> Buy!</MUIButton>
-                            <MUIButton onClick={openTrial1}> Try it!</MUIButton>
+                            <MUIButton> Add to cart!</MUIButton>
                         </MUIButtonGroup>
                     </StyledLadoInfo>
                     <StyledInstructionsDiv inverse={true}>
@@ -40,6 +39,7 @@ function Shop (){
                         <StyledSIImg src="scroll.png"/>
                     </StyledInstructionsDiv>
                 </StyledProductFrame>
+                ))}
                 <StyledProductFrame back={"HD-wallpaper-orange-light-on-black-background-orange-line-background-luminous-background-orange-light-line-background.jpg"} inverse={true}>
                     <article className="producto__ladoInfo">
                         <h1></h1>
@@ -63,7 +63,7 @@ function Shop (){
                 </StyledProductFrame>
             </StyledMain>
         </>
-    )
+    ))
 
 }
 
