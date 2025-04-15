be6a254dec35d40fb39d297a24708faf57373990
demilv <gonzalo.cano@bootcamp-upgrade.com>
2025-04-15 14:38:33 +0200

    creando styled components para el cart

---
+++ src/styledC/cart/cart.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledFrame = styled.div`
+    height: 8em;
+    width:100%;
+    padding: 0 5em;
+    display: flex;
+    flex-direction:row;
+    justify-content:space-between;    
+    align-items:center;    
+    gap:2em
+`
