8e583e0da8733a9f308b173b32761f745c0ca850
demilv <gonzalo.cano@bootcamp-upgrade.com>
2025-05-13 13:29:22 +0200

    styled components para el scroll

--- src/pages/shop/scrolls.tsx
+++ src/pages/shop/scrolls.tsx
@@ -4,9 +4,10 @@ import { BlackScreen } from "../../styledC/generic/Screens"
 interface scrollsInterface {
     showData: (data: string) => void
     onClose: () => void
+    dataToShow: string
 }
 
-const Scrolls: React.FC<scrollsInterface> = ({showData}) => {
+const Scrolls: React.FC<scrollsInterface> = ({showData, onClose, dataToShow}) => {
 
 
     return (
---
+++ src/styledC/shop/scrolls.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const FrameScroll = styled.div`
+    margin: 15%;
+    padding:5% 10%;
+    background-color: #252525;
+    border: 20px solid #FFF0F0;
+    display:flex;
+    justify-content: center;
+    align-items:center
+`
+export const ScrollText = styled.h4`
+    font-family: "VT323"; 
+    color: #e59866;  
+    font-size: 1em
+`
