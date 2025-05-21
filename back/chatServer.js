be15ee4e95a9e84bd153700cf0de4e0c95fdeae0
demilv <gonzalo.cano@bootcamp-upgrade.com>
2025-05-21 14:47:40 +0200

    server llamadas  y creacion

---
+++ back/chatServer.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
